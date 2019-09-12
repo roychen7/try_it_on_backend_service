97e738b08aeffb7f64699d0af5ab242a5edc0cd1
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2019-09-11 21:14:14 -0700

    initialized the package.json and index.js; added google driven API auth and get file names within the index file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import statements
+const { authorize, listFiles } = require("./google_drive_auth");
+const fs = require('fs');
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content), listFiles);
+  });
