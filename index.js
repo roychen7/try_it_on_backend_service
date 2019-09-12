488d33bfc4c6dd290779f0ac34bea8768abec6a9
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2019-09-11 21:44:04 -0700

    added more structure to the files and folders

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Import statements
-const { authorize, listFiles } = require("./google_drive_auth");
+const { authorize, listFiles } = require("./google_api_calls/google_drive_auth");
 const fs = require('fs');
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('./google_api_calls/credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
     authorize(JSON.parse(content), listFiles);
