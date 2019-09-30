c2eab56b122717ca173e92b1e55283201ab03a24
roychen7 <roychen0724@hotmail.com>
2019-09-29 17:41:10 -0700

    post users

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
+'use strict';
+
+const _index = require('./index.js');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/hello_world', function (req, res) {
+  var result = _index.helloWorld();
+  console.log(result);
+  res.send(result);
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port ${port}!')
+app.listen(port, function () {
+  return console.log('App listening on port ' + port + '!');
 });
