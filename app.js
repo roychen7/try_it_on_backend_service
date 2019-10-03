cf82e1f466ca1aec96fe6b03c83476d0dcc77263
roychen7 <roychen0724@hotmail.com>
2019-10-02 18:42:33 -0700

    fixed postUser endpoint; edited README for better use; added some new packages

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 'use strict';
-
+// Import statements from files
 const _index = require('./index.js');
+// Import statements from packages
 const express = require('express');
+// Init express server
 const app = express();
 const port = 3000;
+// Use express settings and features
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/hello_world', function (req, res) {
   var result = _index.helloWorld();
   console.log(result);
   res.send(result);
 });
+
+app.post('/users', async function (req, res) {
+  console.log('app.js::postUser');
+  console.log(req.url);
+  console.log(req.body);
+  if (req.body) {
+    const result = await _index.postUser(req.body);
+    res.send(result);
+  } else {
+    res.send('There is no body');
+  }
+})
 
 app.listen(port, function () {
   return console.log('App listening on port ' + port + '!');
--- index.js
+++ index.js
@@ -1,2 +1,50 @@
+'use strict';
 
+const knex = require('knex');
+const { base64encode, base64decode } = require('nodejs-base64');
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'password',
+        database: 'postgres',
+        port: 5432
+    }
+});
 
+const helloWorld = exports.helloWorld = function helloWorld() {
+    if(db) {
+        console.log("DB connected");
+    }
+    return "Hello World";
+};
+
+const postUser = exports.postUser = async function postUser(body) {
+    console.log('index.js::postUser');
+
+    return db.transaction(trans => {
+        
+        let userid;
+
+        const encoded = base64encode(body.username);
+        console.log(encoded);
+
+        const userRow = {
+            user_id: encoded,
+            first_name: body.first_name,
+            last_name: body.last_name,
+            username: body.username,
+            email: body.email,
+            password: body.password
+        }
+
+        return trans.insert(userRow, 'user_id').into('users').then(
+            ids => {
+                console.log(ids);
+                userid = ids[0];
+                return {message: "user " + userid + " has been created"};
+            }
+        )
+    })
+}
