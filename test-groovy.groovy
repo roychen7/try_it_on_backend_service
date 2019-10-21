4f5da9118139b8fcbf8714db3135d911350fdb02
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2019-10-20 17:40:56 -0700

    testing script 24324

--- test-groovy.groovy
+++ test-groovy.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                echo "Hello World23!"
+                echo "Hello World24322!"
             }
         }
     }
