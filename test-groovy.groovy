8d4a23680951a3d570eff693052d38d9a787f878
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2019-10-27 12:55:48 -0700

    testing if pr jenkins still works

--- test-groovy.groovy
+++ test-groovy.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                echo "Hello World24322fsd43242rwrwegdfgtrdsg!"
+                echo "Hello World - Testing!"
             }
         }
     }
